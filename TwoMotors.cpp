a90fb3747232a78fdc1b5f86654224d9e19ddae9
hugo <hugomaxmontelesteixeira@gmail.com>
2021-09-05 18:10:40 -0300

    Added turnDegrees funtion

--- TwoMotors.cpp
+++ TwoMotors.cpp
@@ -1,7 +1,20 @@
+/*
+  Autor: Hugo Max M. Teixeira
+  Data: 05/2021
+
+  Para o controle de dois motores com encoder;
+  Arquivo .cpp
+
+*/
+
 #include <TwoMotors.h>	// Inclusão do arquivo .h da biblioteca
 
 TwoMotors::TwoMotors(DC_motor_controller* motor1, DC_motor_controller* motor2){	//Construtor que recebe dois objetos do tipo DC_motor_controller (que serão sincronizados)
 	this->m1=motor1; this->m2=motor2;
+}
+
+void TwoMotors::setGyreDegreesRatio(float rot, float ang){
+	this-> rot_per_degree = rot/ang;
 }
 
 void TwoMotors::together(float vel, float rot=0){ // Para a movimentação dos dois motores em uma mesma velocidade e com o mesmo valor de rotações
@@ -24,4 +37,15 @@ void TwoMotors::together(float vel1, float rot1, float vel2, float rot2){	// Par
 	}
 	m1->run(0);
 	m2->run(0);
+}
+
+void TwoMotors::turnDegree(float vel, float degrees){ // For turn angles (in degrees)
+	m1->resetForGyrate();
+	m2->resetForGyrate();
+	float rot = degrees*rot_per_degree;
+	if(degrees > 0){
+		together(vel, rot, -vel, -rot);
+	} else if(degrees < 0){
+		together(-vel, -rot, vel, rot);
+	}
 }
--- TwoMotors.h
+++ TwoMotors.h
@@ -1,9 +1,10 @@
 /*
   Autor: Hugo Max M. Teixeira
   Data: 05/2021
-  
+
   Para o controle de dois motores com encoder;
-  
+  Arquivo .h (de cabeçalho)
+
 */
 
 #ifndef TwoMotors_h
@@ -11,17 +12,20 @@
 
 #include <DC_motor_controller.h>
 
-class TwoMotors{ 
+class TwoMotors{
 public:
-	
+
 	DC_motor_controller* m1 =new DC_motor_controller();
 	DC_motor_controller* m2 =new DC_motor_controller();
-	
+
 	TwoMotors(DC_motor_controller* motor1, DC_motor_controller* motor2);
 	void together(float vel, float rot=0);
 	void together(float vel1, float rot1, float vel2, float rot2);
-	
-	
+	void setGyreDegreesRatio(float rot, float ang);
+	void turnDegree(float vel, float deg);
+
+private:
+	float rot_per_degree;
 };
 
 #endif
